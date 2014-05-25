ac3ff4f6c3566858e0252f8869714908576f0b29
Rich Daley <rich@richd.me>
2014-05-25 09:53:23 +0100

    New theme, search fixes, 10 items per page

--- songlist-viewer/songlist-viewer.js
+++ songlist-viewer/songlist-viewer.js
@@ -20,30 +20,44 @@ if (Meteor.isClient) {
       ]
     },
     pager: {
-      options: [20, 50, 100],
-      itemsPerPage: 20
+      options: [10, 20, 50, 100],
+      itemsPerPage: 10
     },
     filters: {
       artist: {
 	operator: ['$regex', 'i'],
 	condition: '$or',
-	searchable: true
+	searchable: 'required'
       },
       track: {
 	operator: ['$regex', 'i'],
 	condition: '$or',
-	searchable: true
+	searchable: 'required'
       }
     }
   });
 
+  var search_func = function (event) {
+    var search = $('#search').val();
+    SongsFilter.filter.set('artist', {value: search});
+    SongsFilter.filter.set('track', {value: search});
+  };
+
   Template.songs.events({
-    'keyup #search': function (event) {
-      var search = $('#search').val();
-      SongsFilter.filter.set('artist', {value: search});
-      SongsFilter.filter.set('track', {value: search});
+    'keyup #search': search_func,
+    'touchleave #search': search_func,
+    'mouseleave #search': search_func,
+    'click .fc-filter': function (event) {
+      $('#search').val(''); // reset box
+      // because filters only override the artist search
+      SongsFilter.filter.set('track', {value: ''});
+    },
+    'click .fc-filter-reset': function (event) {
+      $('#search').val(''); // reset box
     }
   });
+
+  
 
   Template.songs.alpha = "abcdefghijklmnopqrstuvwxyz".split("");
 }
